0cb68c02a6f3ddb0921a53c5e987e5383819cb02
Linda Chan <cylinda0407@gmail.com>
2017-11-03 00:37:01 -0700

    Complete W5D4 homework - revised

--- W5D4/homework.js
+++ W5D4/homework.js
@@ -1,44 +1,44 @@
-// // Simple Timeout //
-//
-// window.setTimeout(function(){
-//   alert(`HAMMERTIME`);
-// }, 5000);
-//
-// // Timeout Plus Closure //
-//
-// function hammerTime(time){
-//   setTimeout(function () {
-//     alert(`${time} is hammertime!`);
-//   }, time);
-// }
-//
-// Some tea? Some biscuits? //
-//
-// const readline = require('readline');
-// const reader = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-//
-// function askTeaOptions(callback){
-//   //ask first question
-//   reader.question(`Would you like some tea? (y/n)`, function(teaAns) {
-//     //ask second question
-//     reader.question(`Would you like some biscuits? (y/n)`, function(biscuitAns) {
-//       //render Reponse func => giveTeaResponsefunction(teaOpt, bisOpt)
-//       callback(teaAns, biscuitAns);
-//     });
-//   });
-// }
-//
-// const giveTeaResponsefunction = function (teaOpt, bisOpt){
-//   const firstAns = (teaOpt === 'y') ? 'do' : 'don\'t';
-//   const secondAns = (bisOpt === 'y') ? 'do' : 'don\'t';
-//   console.log(`So you ${firstAns} want tea and you ${secondAns} want biscuits.`);
-//   reader.close();
-// };
-//
-// askTeaOptions(giveTeaResponsefunction);
+// Simple Timeout //
+
+window.setTimeout(function(){
+  alert(`HAMMERTIME`);
+}, 5000);
+
+// Timeout Plus Closure //
+
+function hammerTime(time){
+  setTimeout(function () {
+    alert(`${time} is hammertime!`);
+  }, time);
+}
+
+Some tea? Some biscuits? //
+
+const readline = require('readline');
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function askTeaOptions(callback){
+  //ask first question
+  reader.question(`Would you like some tea? (y/n)`, function(teaAns) {
+    //ask second question
+    reader.question(`Would you like some biscuits? (y/n)`, function(biscuitAns) {
+      //render Reponse func => giveTeaResponsefunction(teaOpt, bisOpt)
+      callback(teaAns, biscuitAns);
+    });
+  });
+}
+
+const giveTeaResponsefunction = function (teaOpt, bisOpt){
+  const firstAns = (teaOpt === 'y') ? 'do' : 'don\'t';
+  const secondAns = (bisOpt === 'y') ? 'do' : 'don\'t';
+  console.log(`So you ${firstAns} want tea and you ${secondAns} want biscuits.`);
+  reader.close();
+};
+
+askTeaOptions(giveTeaResponsefunction);
 
 function Cat () {
   this.name = 'Markov';
